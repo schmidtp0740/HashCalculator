a516fb795f35a40b5aabe202553312c11a6e5730
schmidtp0740 <schmidtp0740@gmail.com>
2016-10-19 13:03:45 -0500

    Completed Project where sha256 hash can be calculated

--- HashCheckSum/MainWindow.xaml.cs
+++ HashCheckSum/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
+
 
 namespace HashCheckSum
 {
@@ -23,6 +26,56 @@ namespace HashCheckSum
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Browsebtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                FileLocationText.Text = filename;
+            }
+        }
+
+        private void Calcbtn_Click(object sender, RoutedEventArgs e)
+        {
+            var fileStream = new FileStream(FileLocationText.Text, FileMode.OpenOrCreate,
+                FileAccess.Read);
+            string result = GetChecksumBuffered(fileStream);
+
+            HashLbl.Content += result;
+
+            if (result == HashText.Text)
+            {
+                VerifyLbl.Content = "File is Authentic";
+            }
+            else
+            {
+                VerifyLbl.Content = "File is not Authentic";
+            }
+        }
+
+        private static string GetChecksumBuffered(Stream stream)
+        {
+            using (var bufferedStream = new BufferedStream(stream, 1024 * 32))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(bufferedStream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+            }
         }
     }
 }
